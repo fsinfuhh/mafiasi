0eb19e122f5ddeef419bff993cd961fcd0ca956e
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-18 19:35:33 +0200

    Update urls.py for new apps and login/logout

--- mafiasi/urls.py
+++ mafiasi/urls.py
@@ -4,6 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # url(r'^$', 'mafiasi.views.home', name='home'),
+    url(r'^registration/', include('mafiasi.registration.urls')),
+    url(r'^dashboard/', include('mafiasi.dashboard.urls')),
+
+    url(r'^login$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login',
+            name='logout'),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
