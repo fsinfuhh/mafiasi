5afafd1d8210dc2279ea7113dab35dc19d2b96d9
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-18 15:48:52 +0200

    Add base app with mafiasi user model

---
+++ mafiasi/base/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from mafiasi.base.models import Yeargroup, PasswdEntry, Mafiasi
+
+admin.site.register(Yeargroup)
+admin.site.register(PasswdEntry)
+admin.site.register(Mafiasi)
---
+++ mafiasi/base/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class YeargroupManager(models.Manager):
+    def get_by_account(self, account):
+        try:
+            passwd = PasswdEntry.objects.get(username=account)
+            try:
+                return Yeargroup.objects.get(gid=passwd.gid)
+            except Yeargroup.DoesNotExist:
+                return None
+        except PasswdEntry.DoesNotExist:
+            return None
+
+
+class Yeargroup(models.Model):
+    slug = models.SlugField(max_length=16)
+    name = models.CharField(max_length=16)
+    gid = models.IntegerField(blank=True, null=True)
+
+    objects = YeargroupManager()
+
+    def __unicode__(self):
+        return self.name
+
+
+class PasswdEntry(models.Model):
+    username = models.CharField(max_length=40, unique=True)
+    full_name = models.CharField(max_length=60)
+    gid = models.IntegerField()
+
+    def __unicode__(self):
+        return self.username
+
+
+class Mafiasi(AbstractUser):
+    account = models.CharField(max_length=40)
+    yeargroup = models.ForeignKey(Yeargroup, blank=True, null=True)
+
