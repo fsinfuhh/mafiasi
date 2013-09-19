3685f8f4ed512890aa13ede189764589ad0c111a
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-19 17:29:53 +0200

    registration: Fix password equivalence check

--- mafiasi/registration/forms.py
+++ mafiasi/registration/forms.py
@@ -37,7 +37,7 @@ class PasswordForm(forms.Form):
         max_length=MAXIMUM_PASSWORD_LENGTH,
     )   
 
-    def clean_new_password2(self):
+    def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
         if password1 and password2:
