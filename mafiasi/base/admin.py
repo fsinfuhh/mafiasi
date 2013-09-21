94bcf743d2fb23c98be2b03466f3a083d4bf83d7
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-21 16:08:51 +0200

    base: Better admin interface

--- mafiasi/base/admin.py
+++ mafiasi/base/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 
 from mafiasi.base.models import Yeargroup, PasswdEntry, Mafiasi
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+class PasswdEntryAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'full_name']
+
+class MafiasiChangeForm(UserChangeForm):
+    class Meta:
+        model = Mafiasi
+
+class MafiasiAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional info', {'fields': ('account', 'yeargroup')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional info', {'fields': ('account', 'yeargroup')}),
+    )
+    form = MafiasiChangeForm
+
 admin.site.register(Yeargroup)
-admin.site.register(PasswdEntry)
-admin.site.register(Mafiasi)
+admin.site.register(PasswdEntry, PasswdEntryAdmin)
+admin.site.register(Mafiasi, MafiasiAdmin)
