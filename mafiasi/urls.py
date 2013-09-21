33443764c279f1d95fa899884f1b4c21f3bf012d
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-22 00:35:14 +0200

    Redirect to / after logout

--- mafiasi/urls.py
+++ mafiasi/urls.py
@@ -12,8 +12,9 @@ urlpatterns = patterns('',
     url(r'^mumble/', include('mafiasi.mumble.urls')),
 
     url(r'^login$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout_then_login',
-            name='logout'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {
+        'next_page': '/'
+    }, name='logout'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
