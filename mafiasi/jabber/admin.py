591ca6f72bc6c8e741491a8168c097ad471dc276
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-24 19:02:18 +0200

    jabber: Use only own models in admin.py

--- mafiasi/jabber/admin.py
+++ mafiasi/jabber/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
 
-from mafiasi.jabber.models import (PrivacyDefaultList, PrivacyList,
-        PrivacyListData, PrivateStorage, Rostergroups, Rosteruser,
-        SrGroup, SrUser, JabberUser, Vcard, JabberUserMapping,
+from mafiasi.jabber.models import (DefaultGroup, JabberUserMapping,
         YeargroupSrGroupMapping)
 
-admin.site.register(PrivacyDefaultList)
-admin.site.register(PrivacyList)
-admin.site.register(PrivacyListData)
-admin.site.register(PrivateStorage)
-admin.site.register(Rostergroups)
-admin.site.register(Rosteruser)
-admin.site.register(SrGroup)
-admin.site.register(SrUser)
-admin.site.register(JabberUser)
-admin.site.register(Vcard)
+admin.site.register(DefaultGroup)
 admin.site.register(JabberUserMapping)
 admin.site.register(YeargroupSrGroupMapping)
