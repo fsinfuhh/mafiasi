e33f29af03760371449b927b34318b11cb29ba53
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-24 19:01:52 +0200

    jabber: Implement account creation and password reset

--- mafiasi/jabber/urls.py
+++ mafiasi/jabber/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('mafiasi.jabber.views',
     url(r'^$', 'index', name='jabber_index'),
     url(r'^create$', 'create', name='jabber_create'),
+    url(r'^password_reset$', 'password_reset', name='jabber_password_reset'),
 )
--- mafiasi/jabber/views.py
+++ mafiasi/jabber/views.py
@@ -1,8 +1,12 @@
 from django.template.response import TemplateResponse
+from django.shortcuts import redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
-from mafiasi.jabber.models import get_account
+from mafiasi.registration.forms import CheckPasswordForm
+from mafiasi.jabber.models import get_account, create_account
 
 def index(request):
     jabber_user = get_account(request.user)
@@ -15,6 +19,45 @@ def index(request):
 
 @login_required
 def create(request):
-    return TemplateResponse(request, 'jabber/create.html', {
+    if get_account(request.user) is not None:
+        return redirect('jabber_password_reset')
+    
+    if request.method == 'POST':
+        form = CheckPasswordForm(request.POST, user=request.user)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            status, user = create_account(request.user, password)
+            if status == 'created':
+                messages.success(request, _('Account was created.'))
+            elif status == 'exists':
+                messages.warning(request, _('Account already exists.'))
+            else:
+                messages.error(request, _('Sorry, we had an internal error.'))
+            return redirect('jabber_index')
+    else:
+        form = CheckPasswordForm(user=request.user)
 
+    return TemplateResponse(request, 'jabber/create.html', {
+        'form': form
+    })
+
+@login_required
+def password_reset(request):
+    account = get_account(request.user)
+    if account is None:
+        return redirect('jabber_create')
+
+    if request.method == 'POST':
+        form = CheckPasswordForm(request.POST, user=request.user)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            account.password = password
+            account.save()
+            messages.success(request, _("Password was changed."))
+            return redirect('jabber_index')
+    else:
+        form = CheckPasswordForm(user=request.user)
+
+    return TemplateResponse(request, 'jabber/password_reset.html', {
+        'form': form
     })
