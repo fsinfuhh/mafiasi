eb6c481177cfc1b37a634b47dfd2ebd186236894
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-09-28 14:13:35 +0200

    base: Mafiasi expose new_password on set_password call for signals

--- mafiasi/base/models.py
+++ mafiasi/base/models.py
@@ -36,7 +36,18 @@ class PasswdEntry(models.Model):
 class Mafiasi(AbstractUser):
     account = models.CharField(max_length=40)
     yeargroup = models.ForeignKey(Yeargroup, blank=True, null=True)
+    new_password = None
     
     def is_student(self):
         return self.account and (self.account[0].isdigit() or
                                  self.account[0] == u'x')
+
+    def set_password(self, new_password):
+        """ Set attribute new_password after changing a password.
+
+        This way other parts of this app can register to Mafiasi.post_save
+        signal and access the plaintext password for changing it in their
+        service.
+        """
+        super(Mafiasi, self).set_password(new_password)
+        self.new_password = new_password
