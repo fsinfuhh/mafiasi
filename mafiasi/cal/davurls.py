0ccc26946fba20aedfde8e1364dc55aff99972f3
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-10-20 18:35:42 +0200

    cal: Proxy collections, some refactoring

--- mafiasi/cal/davurls.py
+++ mafiasi/cal/davurls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('mafiasi.cal.views',
     url(r'^$', 'index', name='cal_davindex'),
-    url(r'^([^/]+)/([a-zA-Z0-9_-]+)\.(ics|vcf)/(.*)', 'proxy_request',
+    url(r'^([A-Za-z0-9_.-]+)/$', 'proxy_request_collection',
+            name='cal_proxy_request_collection'),
+    url(r'^([A-Za-z0-9_.-]+)/([a-zA-Z0-9_-]+)\.(ics|vcf)/(.*)', 'proxy_request',
             name='cal_proxy_request'),
 )
