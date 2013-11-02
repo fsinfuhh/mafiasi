a842104e97ed8e88e5a8cd515e0dc47e5720651a
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-11-02 22:58:33 +0100

    Serve media files at /media if DEBUG = True

--- mafiasi/urls.py
+++ mafiasi/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.shortcuts import redirect
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -26,3 +27,10 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT
+        }),
+    )
