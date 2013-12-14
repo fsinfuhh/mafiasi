ab72b6f25766f7393efc57c048c2de111d98937e
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-12-14 23:41:04 +0100

    base: Add templatetag for user formatting

---
+++ mafiasi/base/templatetags/userformat.py
@@ -0,0 +1,21 @@
+from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.simple_tag
+def format_user(user, format='full'):
+    if format == 'full':
+        full_name = conditional_escape(user.get_full_name())
+        username = conditional_escape(user.username)
+        result = u'{0} <span class="user-username">({1})</span>'.format(
+                full_name, username)
+    elif format == 'name':
+        result = conditional_escape(user.get_full_name())
+    elif format == 'username':
+        result = conditional_escape(user.username)
+    else:
+        raise ValueError("Invalid format for format_user")
+
+    return mark_safe(result)
