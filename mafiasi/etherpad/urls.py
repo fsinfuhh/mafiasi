f3453c9341db654196f95bbcbad78e4e1f435a9e
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-12-18 17:17:48 +0100

    etherpad: More permissive pad names in url

--- mafiasi/etherpad/urls.py
+++ mafiasi/etherpad/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('mafiasi.etherpad.views',
     url(r'^$', 'index', name='ep_index'),
     url(r'^create_new_pad$', 'create_new_pad', name='ep_create_new_pad'),
-    url(r'^pad/([A-Za-z0-9\-_]+)/([A-Za-z0-9\-_]+)$', 'show_pad', name='ep_show_pad'),
+    url(r'^pad/([A-Za-z0-9\-_]+)/(.+)$', 'show_pad', name='ep_show_pad'),
 )
