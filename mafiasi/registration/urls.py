05403373c1a106f7b6e2992ba1bb16382308e4f1
Henning Pridoehl <henning.pridoehl@innogames.de>
2013-12-19 18:32:20 +0100

    registration: Add names to all urls

--- mafiasi/registration/urls.py
+++ mafiasi/registration/urls.py
@@ -12,9 +12,11 @@ urlpatterns = patterns('mafiasi.registration.views',
 
 urlpatterns += patterns('django.contrib.auth.views',
     url(r'^password_reset/$', 'password_reset', name='password_reset'),
-    url(r'^password_reset/done$', 'password_reset_done'),
+    url(r'^password_reset/done$', 'password_reset_done',
+            name='password_reset_done'),
     url(r'^password_reset/confirm/(?P<uidb36>[a-z0-9-]+)/(?P<token>[a-z0-9-]+)$',
-            'password_reset_confirm'),
-    url(r'^password_reset/complete', 'password_reset_complete')
+            'password_reset_confirm', name='password_reset_confirm'),
+    url(r'^password_reset/complete', 'password_reset_complete',
+            name='password_reset_complete')
 )
 
