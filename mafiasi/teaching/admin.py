cd027995e4bd1ccf650da71c4e1423d236a23c6c
Damian Hofmann <picobyte@gmx.com>
2014-08-09 17:29:24 +0200

    teaching: Add faculty and department information

--- mafiasi/teaching/admin.py
+++ mafiasi/teaching/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from mafiasi.teaching.models import Teacher, Course, AltCourseName
+from mafiasi.teaching.models import Faculty, Department, Teacher, Course, \
+                                    AltCourseName
 
+admin.site.register(Faculty)
+admin.site.register(Department)
 admin.site.register(Teacher)
 admin.site.register(Course)
 admin.site.register(AltCourseName)
