8c03ec76874313c64c11d6a6ebe97d69ed2991fc
Henning Pridöhl <henning.pridoehl@innogames.com>
2014-10-29 22:51:21 +0100

    pks: Trustgraph command accepts arguments which graphs to build

--- mafiasi/pks/management/commands/trustgraph.py
+++ mafiasi/pks/management/commands/trustgraph.py
@@ -2,23 +2,27 @@ import os
 
 from PIL import Image
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from mafiasi.pks.models import KeysigningParty
 from mafiasi.pks.graph import generate_graph
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Generate the trust graph'
 
-    def handle_noargs(self, **options):
-        self._build_graph('global')
+    def handle(self, *args, **options):
+        if not args or 'global' in args:
+            self._build_graph('global')
+        
         for party in KeysigningParty.objects.all():
+            graph_name = 'party{}'.format(party.pk)
+            if args and graph_name not in args:
+                continue
             key_fingerprints = []
             for participant in party.participants.all():
                 for key in participant.keys.all():
                     key_fingerprints.append(key.fingerprint)
-            graph_name = 'party{}'.format(party.pk)
             self._build_graph(graph_name, key_fingerprints)
     
     def _build_graph(self, graph_name, restrict_keys=None):
