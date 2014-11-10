e733719352976a8cd1601092d37ff2ccde22085b
Henning Pridöhl <henning.pridoehl@innogames.com>
2014-11-10 22:30:31 +0100

    base: Merge migrations

---
+++ mafiasi/base/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0002_mafiasi_is_guest'),
+        ('base', '0002_auto_20141110_1506'),
+    ]
+
+    operations = [
+    ]
