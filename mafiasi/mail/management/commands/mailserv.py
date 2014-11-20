6b09456b245bd3a7fb405ed49f9a0c0a405fdee7
Henning Pridöhl <henning.pridoehl@innogames.com>
2014-11-20 18:02:32 +0100

    mail/mailinglist: Put all mailservers into one daemon

--- mafiasi/mail/management/commands/mailserv.py
+++ mafiasi/mail/management/commands/mailserv.py
@@ -3,14 +3,16 @@ import signal
 import sys
 
 from django.core.management.base import NoArgsCommand
-from django.conf import settings
 
-from mafiasi.mailinglist.server import MailinglistServer
+from mafiasi.mail.signals import CollectServer, collect_servers
 
 class Command(NoArgsCommand):
     help = 'Starts the SMTP server for mailinglists'
 
     def handle_noargs(self, *args, **options):
         signal.signal(signal.SIGINT, lambda sig, frame: sys.exit(0))
-        MailinglistServer(settings.MAILINGLIST_SERVER, None)
+        servers = []
+        collect_servers.send_robust(sender=CollectServer, servers=servers)
+        for server_class, server_args in servers:
+            server_class(*server_args)
         asyncore.loop()
--- mafiasi/mail/signals.py
+++ mafiasi/mail/signals.py
@@ -3,4 +3,8 @@ import django.dispatch
 class CollectMail(object):
     pass
 
+class CollectServer(object):
+    pass
+
 collect_mailaddresses = django.dispatch.Signal(providing_args=["addresses"])
+collect_servers = django.dispatch.Signal(providing_args=["servers"])
