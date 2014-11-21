f74c17ad8f9cd4fa97b2da9f52a9f38747286162
Henning Pridöhl <henning.pridoehl@innogames.com>
2014-11-22 00:30:57 +0100

    mail: Send mailaddresses_known signal when mailhost requests mailaddresses

--- mafiasi/mail/signals.py
+++ mafiasi/mail/signals.py
@@ -7,4 +7,5 @@ class CollectServer(object):
     pass
 
 collect_mailaddresses = django.dispatch.Signal(providing_args=["addresses"])
+mailaddresses_known = django.dispatch.Signal()
 collect_servers = django.dispatch.Signal(providing_args=["servers"])
--- mafiasi/mail/views.py
+++ mafiasi/mail/views.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.template.response import HttpResponse
 
 from mafiasi.base.decorators import require_auth
-from mafiasi.mail.signals import CollectMail, collect_mailaddresses
+from mafiasi.mail.signals import (CollectMail, collect_mailaddresses,
+                                  mailaddresses_known)
 
 @require_auth(username='mail', password=settings.EMAIL_ADDRESSES_PASSWORD)
 def mailaddresses(request):
@@ -13,5 +14,6 @@ def mailaddresses(request):
     collect_mailaddresses.send_robust(sender=CollectMail, addresses=addresses)
     addresses += settings.VALID_EMAIL_ADDRESSES
     output = '\n'.join(address + ' OK' for address in addresses) + '\n'
+    mailaddresses_known.send_robust(sender=CollectMail)
     return HttpResponse(output.encode('utf-8'), content_type='text/plain')
 
