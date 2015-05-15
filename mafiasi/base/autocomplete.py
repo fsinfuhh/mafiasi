5fac9e29264df4444187f4701d04e630b921c405
Henning Pridöhl <henning@hprid.de>
2015-05-15 03:24:46 +0200

    base: Make autocomplete behavior less strange

--- mafiasi/base/autocomplete.py
+++ mafiasi/base/autocomplete.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from mafiasi.base.models import Mafiasi
 
-RESULT_LIMIT = 5
+RESULT_LIMIT = 10
 MIN_LENGTH = 3
 
 def autocomplete_users(term):
@@ -14,15 +14,21 @@ def autocomplete_users(term):
     always be MIN_LENGTH long.
 
     For queries returning more than RESULT_LIMIT entries, entries are
-    ignored. Sometimes this results in strange results, but makes
-    crawling the database harder.
+    ignored, except there is a username matching exactly.
     """
     terms = term.strip().lower().split()
     if len(terms) == 0:
         return []
+        
+    # Always make sure we "autocomplete" a complete match for username
+    try:
+        exact_user = Mafiasi.objects.get(username=terms[0])
+        autocomplete_refused = [exact_user]
+    except Mafiasi.DoesNotExist:
+        autocomplete_refused = []
 
     if len(terms[0]) < MIN_LENGTH:
-        return []
+        return autocomplete_refused
 
     if len(terms) == 1:
         users = []
@@ -30,15 +36,10 @@ def autocomplete_users(term):
         _add_nodup('username', terms[0], users, dups)
         _add_nodup("regexp_replace(username, '^([0-9]+|x)', '')", terms[0], users, dups)
         _add_nodup('lower(first_name)', terms[0], users, dups)
-        _add_nodup('lower(last_name)', term[0], users, dups)
+        _add_nodup('lower(last_name)', terms[0], users, dups)
         
-        # Always make sure we "autocomplete" a complete match for username
-        try:
-            exact_user = Mafiasi.objects.get(username=terms[0])
-            if exact_user.pk not in dups:
-                users.append(exact_user)
-        except Mafiasi.DoesNotExist:
-            pass
+        if len(users) > RESULT_LIMIT:
+            return autocomplete_refused
 
     else:
         query = ('SELECT * FROM base_mafiasi '
@@ -47,7 +48,7 @@ def autocomplete_users(term):
         params = (terms[0] + '%', terms[1] + '%', RESULT_LIMIT + 1)
         users = list(Mafiasi.objects.raw(query, params=params))
         if len(users) > RESULT_LIMIT:
-            users = []
+            return autocomplete_refused
 
     users.sort(key=_sort_key)
     return users
@@ -57,12 +58,11 @@ def _add_nodup(expr, term, users, dups):
     params = (term + '%', RESULT_LIMIT + 1)
     special_users = list(Mafiasi.objects.raw(query, params=params))
     
-    if len(special_users) <= RESULT_LIMIT:
-        for user in special_users:
-            if user.pk in dups:
-                continue
-            users.append(user)
-            dups.add(user.pk)
+    for user in special_users:
+        if user.pk in dups:
+            continue
+        users.append(user)
+        dups.add(user.pk)
 
 def _sort_key(user):
     return (user.first_name, user.last_name, user.username)
