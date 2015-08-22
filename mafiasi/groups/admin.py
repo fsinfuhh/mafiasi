842c79e5e2ecb233fc824e5e0e5fcff08339c70d
Mafiasi <server-ag@informatik.uni-hamburg.de>
2015-08-22 21:04:26 +0200

    groups: Filter admin users in admin view

--- mafiasi/groups/admin.py
+++ mafiasi/groups/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from mafiasi.groups.models import GroupInvitation, GroupProperties
 
+class GroupPropertiesAdmin(admin.ModelAdmin):
+    filter_vertical = ['admins']
+
 admin.site.register(GroupInvitation)
-admin.site.register(GroupProperties)
+admin.site.register(GroupProperties, GroupPropertiesAdmin)
