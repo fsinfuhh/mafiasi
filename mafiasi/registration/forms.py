a00eba130e3052fe0be8dce99735f0ff6c0b9a3a
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-10-07 10:19:15 +0200

    Add extra_email_context to PasswordResetForm

--- mafiasi/registration/forms.py
+++ mafiasi/registration/forms.py
@@ -160,7 +160,8 @@ class PasswordResetForm(forms.Form):
              subject_template_name='registration/password_reset_subject.txt',
              email_template_name='registration/password_reset_email.html',
              use_https=False, token_generator=default_token_generator,
-             from_email=None, request=None, html_email_template_name=None):
+             from_email=None, request=None, html_email_template_name=None,
+             extra_email_context=None):
         """
         Generates a one-use only link for resetting password and sends to the
         user.
@@ -190,6 +191,9 @@ class PasswordResetForm(forms.Form):
                 'token': token_generator.make_token(user),
                 'protocol': 'https' if use_https else 'http',
             }
+            if extra_email_context is not None:
+                c.update(extra_email_context)
+
             subject = loader.render_to_string(subject_template_name, c)
             # Email subject *must not* contain newlines
             subject = ''.join(subject.splitlines())
