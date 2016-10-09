59d628c2fb092df16ee3219b44d03fcd217f9534
Pascal Wichmann <wichmannpas@gmail.com>
2016-10-09 19:07:28 +0200

    Implemented etherpad pinning

--- mafiasi/etherpad/urls.py
+++ mafiasi/etherpad/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .views import index, create_new_pad, show_pad, show_pad_html, delete_pad
+from .views import index, create_new_pad, show_pad, show_pad_html, pin_pad, delete_pad
 
 urlpatterns = [
     url(r'^$', index, name='ep_index'),
     url(r'^create_new_pad$', create_new_pad, name='ep_create_new_pad'),
     url(r'^pad/([A-Za-z0-9\-_]+)/(.+)$', show_pad, name='ep_show_pad'),
     url(r'^html/pad/([A-Za-z0-9\-_]+)/(.+)$', show_pad_html, name='ep_show_pad_html'),
+    url(r'^pin/pad/([A-Za-z0-9\-_]+)/(.+)$', pin_pad, name='ep_pin_pad'),
     url(r'^delete/([A-Za-z0-9\-_]+)/(.+)$', delete_pad, name='ep_delete_pad'),
 ]
