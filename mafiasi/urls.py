0b3b233a7f770524fe6fe488cc2f8bb234dc24bf
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2016-10-26 14:48:05 +0200

    urlconf loading only if app ist installed

--- mafiasi/urls.py
+++ mafiasi/urls.py
@@ -5,10 +5,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-import mafiasi.base.urls, mafiasi.registration.urls, mafiasi.dashboard.urls, mafiasi.discourse.urls,\
+import mafiasi.base.urls, mafiasi.registration.urls, mafiasi.dashboard.urls, \
     mafiasi.mumble.urls, mafiasi.wiki.urls, mafiasi.groups.urls,\
-    mafiasi.etherpad.urls, mafiasi.gprot.urls, mafiasi.teaching.urls, mafiasi.mail.urls, mafiasi.mailinglist.urls,\
-    mafiasi.guests.urls, mafiasi.owncloud.urls
+    mafiasi.etherpad.urls, mafiasi.guests.urls, mafiasi.owncloud.urls
 
 import django.contrib.auth.views
 import django.conf.urls.i18n
@@ -22,15 +21,8 @@ urlpatterns = [
     url(r'^base/', include(mafiasi.base.urls)),
     url(r'^registration/', include(mafiasi.registration.urls)),
     url(r'^dashboard/', include(mafiasi.dashboard.urls)),
-    url(r'^discourse/', include(mafiasi.discourse.urls)),
-    url(r'^mumble/', include(mafiasi.mumble.urls)),
-    url(r'^wiki/', include(mafiasi.wiki.urls)),
     url(r'^groups/', include(mafiasi.groups.urls)),
     url(r'^etherpad/', include(mafiasi.etherpad.urls)),
-    url(r'^gprot/', include(mafiasi.gprot.urls)),
-    url(r'^teaching/', include(mafiasi.teaching.urls)),
-    url(r'^mail/', include(mafiasi.mail.urls)),
-    url(r'^mailinglist/', include(mafiasi.mailinglist.urls)),
     url(r'^guests/', include(mafiasi.guests.urls)),
     url(r'^owncloud/', include(mafiasi.owncloud.urls)),
 
@@ -44,6 +36,48 @@ urlpatterns = [
     url(r'^admin/doc/', include(django.contrib.admindocs.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if 'mafiasi.mail' in settings.INSTALLED_APPS:
+    import mafiasi.mail.urls
+    urlpatterns += [
+        url(r'^mail/', include(mafiasi.mail.urls)),
+    ]
+
+if 'mafiasi.mailinglist' in settings.INSTALLED_APPS:
+    import mafiasi.mailinglist.urls
+    urlpatterns += [
+        url(r'^mailinglist/', include(mafiasi.mailinglist.urls)),
+    ]
+
+if 'mafiasi.wiki' in settings.INSTALLED_APPS:
+    import mafiasi.wiki.urls
+    urlpatterns += [
+        url(r'^wiki/', include(mafiasi.wiki.urls)),
+    ]
+
+if 'mafiasi.mumble' in settings.INSTALLED_APPS:
+    import mafiasi.mumble.urls
+    urlpatterns += [
+        url(r'^mumble/', include(mafiasi.mumble.urls)),
+    ]
+
+if 'mafiasi.discourse' in settings.INSTALLED_APPS:
+    import mafiasi.discourse.urls
+    urlpatterns += [
+        url(r'^discourse/', include(mafiasi.discourse.urls)),
+    ]
+
+if 'mafiasi.teaching' in settings.INSTALLED_APPS:
+    import mafiasi.teaching.urls
+    urlpatterns += [
+        url(r'^teaching/', include(mafiasi.teaching.urls)),
+    ]
+
+if 'mafiasi.gprot' in settings.INSTALLED_APPS:
+    import mafiasi.gprot.urls
+    urlpatterns += [
+        url(r'^gprot/', include(mafiasi.gprot.urls)),
+    ]
 
 if 'mafiasi.pks' in settings.INSTALLED_APPS:
     import mafiasi.pks.urls
