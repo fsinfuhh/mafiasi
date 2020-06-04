e044a2cc7b4d9c5f75e9434aef87e050c6655993
Timon Engelke <timon.engelke@online.de>
2020-06-04 13:43:00 +0200

    fix fb18

--- mafiasi/fb18/apps.py
+++ mafiasi/fb18/apps.py
@@ -8,5 +8,5 @@ class Fb18Config(BaseService):
     verbose_name = 'fb18'
     title = _('FB18')
     description = _('FB18 is our deprecated bulletin board, which is kept here as an archive.')
-    link = 'https://fb18.de'
-    image = 'img/services/bitpoll.png'
+    link = 'https://archiv.mafiasi.de/forum/fb18/'
+    image = 'img/services/fb18.png'
