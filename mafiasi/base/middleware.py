0be329ceeaf6fd725b79fa9b4af9b681f7532938
Timon Engelke <timon.engelke@online.de>
2020-10-28 15:19:59 +0100

    show a warning when the email address is invalid

---
+++ mafiasi/base/middleware.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
+from mafiasi.base.models import Mafiasi
+
+
+class InvalidMailMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            if Mafiasi.objects.get(username=request.user.username).real_email.endswith(settings.INVALID_MAIL_DOMAIN):
+                messages.error(request, _('Your email address was automatically set to an invalid one. Please update your email address immediately.'))
+        return self.get_response(request)
+
