b71afe45d8f2e9be791d5b39443ea93f0269fa3f
Timon Engelke <timon.engelke@online.de>
2021-03-25 13:45:58 +0100

    Make adding of Yeargroup -> sr group mapping more comfortable

--- mafiasi/jabber/admin.py
+++ mafiasi/jabber/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django import forms
 
 from mafiasi.jabber.models import (DefaultGroup, JabberUserMapping,
         YeargroupSrGroupMapping)
+from mafiasi.base.models import Yeargroup
+
+class YeargroupSrGroupMappingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        yeargroups = Yeargroup.objects.values_list('id', 'name')
+        self.fields['yeargroup_id'] = forms.ChoiceField(choices=yeargroups)
+
+
+class YeargroupSrGroupMappingAdmin(admin.ModelAdmin):
+    form = YeargroupSrGroupMappingForm
+
 
 admin.site.register(DefaultGroup)
 admin.site.register(JabberUserMapping)
-admin.site.register(YeargroupSrGroupMapping)
+admin.site.register(YeargroupSrGroupMapping, YeargroupSrGroupMappingAdmin)
