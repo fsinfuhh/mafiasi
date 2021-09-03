628d135078ba731a280e85b140ad83a2d0c09016
Timon Engelke <timon.engelke@online.de>
2021-09-03 22:09:00 +0200

    erlangparser: avoid possible code injections

--- mafiasi/jabber/erlangparser.py
+++ mafiasi/jabber/erlangparser.py
@@ -1,8 +1,9 @@
 import json
 import re
+from typing import Union
 
 
-def parse(string):
+def parse(string: str) -> dict:
     # To make the data json, the headers have to be quoted and the angle brackets have to be removed
     # i.e. {key, <<"value">>} to {"key": "value"}
     quoted_headers = re.sub(r"\{(\w+),", r'{"\1":', string)
@@ -16,9 +17,16 @@ def parse(string):
     return result
 
 
-def dump(object):
+def _sanitize(object: Union[list, str]) -> Union[list, str]:
+    # To avoid possible code injections, keep only letters, digits, and whitespace
+    if isinstance(object, list):
+        return [_sanitize(x) for x in object]
+    return re.sub(r'[^\w\s]', '', object)
+
+
+def dump(object: dict) -> str:
     # First, the merged dictionary has to be split in a list of one-element dictionaries
-    data = [{k: v} for k, v in object.items()]
+    data = [{_sanitize(k): _sanitize(v)} for k, v in object.items()]
     # Then, the data is stringified and the transformations from before (header quotes, angle brackets) are reverted
     string = json.dumps(data)
     no_quoted_headers = re.sub(r'\{"(\w+)":', r'{\1,', string)
