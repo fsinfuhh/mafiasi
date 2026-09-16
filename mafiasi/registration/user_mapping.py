3d6bf20137bc0f6f7789dc250f0c5107fd479ab3
22lieber <22lieber@cloak.mafiasi.de>
2026-09-16 17:22:33 +0200

    user mapping federated user info

--- mafiasi/registration/user_mapping.py
+++ mafiasi/registration/user_mapping.py
@@ -19,17 +19,13 @@ class MafiasiUserMapper(UserMapper):
 
     def handle_federated_userinfo(self, user_data: FederatedUserData) -> MafiasiUser:
         # if there is already a user with this username, we create the openid association if it does not exist yet
-        """try:
+        try:
             user = MafiasiUser.objects.get(username=user_data.preferred_username)
-            OpenidUser.objects.get_or_create(
-                sub=user_data.sub,
-                defaults={
-                    "user": user,
-                },
-            )
         except MafiasiUser.DoesNotExist:
-            # if the user does not exist, it should not be created and and error is raised
             raise AssertionError(
-                f"User {user_data.preferred_username} does not exist in local database even though users are only ever created from the dashboard"
-            )"""
-        return super().handle_federated_userinfo(user_data)
+                f"User {user_data.preferred_username} does not exist in local "
+                "database even though users are only ever created from the dashboard"
+            )
+
+        OpenidUser.objects.get_or_create(sub=user_data.sub, defaults={"user": user})
+        return user
