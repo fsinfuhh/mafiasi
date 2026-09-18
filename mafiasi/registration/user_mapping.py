a29536ef44635f3d765c694bc965d25bf183442e
piushartmann <pius.hartmann@studium.uni-hamburg.de>
2026-09-18 20:41:59 +0200

    map user attributes from oidc correctly

--- mafiasi/registration/user_mapping.py
+++ mafiasi/registration/user_mapping.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.conf import settings
+from django.contrib.auth.models import Group
 from simple_openid_connect.integrations.django.models import OpenidUser
 from simple_openid_connect.integrations.django.user_mapping import (
     FederatedUserData,
@@ -11,27 +12,32 @@ from mafiasi.base.models import Mafiasi as MafiasiUser
 
 
 class MafiasiUserMapper(UserMapper):
+    def map_user_attrs(self, user: MafiasiUser, user_data: FederatedUserData) -> None:
+        user.username = user_data.username
+        user.first_name = user_data.fn or ""
+        user.last_name = user_data.sn or ""
+        user.email = user_data.email or ""
+        user.real_email = user_data.realmail or None
+
+    def sync_group_memberships(self, user: MafiasiUser, groups: list[str]) -> None:
+        local_groups = [Group.objects.get_or_create(name=group_name)[0] for group_name in groups if group_name]
+        user.groups.set(local_groups)
+
     def automap_user_attrs(self, user: MafiasiUser, user_data: FederatedUserData) -> None:
         super().automap_user_attrs(user, user_data)
+        self.map_user_attrs(user, user_data)
+        groups = list(getattr(user_data, "groups", []))
+        self.sync_group_memberships(user, groups)
         if settings.OPENID_SYNC_SUPERUSER:
-            groups = getattr(user_data, "groups", [])
             is_superuser = settings.OPENID_SUPERUSER_GROUP in groups
             user.is_superuser = is_superuser
             user.is_staff = is_superuser
 
     def handle_federated_userinfo(self, user_data: FederatedUserData) -> MafiasiUser:
         user, created = MafiasiUser.objects.get_or_create(
-            username=user_data.preferred_username,
-            defaults={
-                "account": "",
-                "email": user_data.email or "",
-                "real_email": user_data.email or None,
-            },
+            username=user_data.username,
+            defaults={"account": ""},
         )
-
-        if created:
-            user.email = user_data.email or ""
-            user.real_email = user_data.email or None
 
         openid_user = OpenidUser.objects.filter(user=user).first()
         if openid_user is None:
